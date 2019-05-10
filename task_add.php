850ca1f22374de37de878f592a688b2f344ffe97
Ali_Safari <AliSafari.Dev@Gmail.com>
2019-05-11 03:05:18 +0430

    Add Task Done.

--- task_add.php
+++ task_add.php
@@ -4,11 +4,45 @@ $tab = 'add_task';
 require './bootstrapper.php';
 require './header.php';
 ?>
+<?php
+if(isset($_POST['add_task'])) {
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $created_at = $_POST['created_at'];
+    if(isset($_POST['status']) && $_POST['status'] == 'done')
+        $status = 1;
+    else
+        $status = 0;
+    $err = [];
+    if($title == null || $content == null || $created_at == null) {
+        $err[] = 'همه فیلدها الزامی است';
+    }
+    if($created_at != null && !is_numeric($created_at))
+        $err[] = 'تاریخ اشتباه است';
+    if(count($err) == 0) {
+        $query = $conn->prepare("insert into task(user_id, title, content, status, created_at) values(:user_id, :title, :content, :status, :created_at)");
+        $query->bindParam(':user_id', $user['id']);
+        $query->bindParam(':title', $title);
+        $query->bindParam(':content', $content);
+        $query->bindParam(':status', $status);
+        $query->bindParam(':created_at', $created_at);
+
+        if($query->execute())
+            echo "<div class=\"alert success\">تسک مورد نظر با موفقیت اضافه شد.</div>";
+        else
+            echo "<div class=\"alert danger\">خطایی رخ داد.</div>";
+    } else {
+        foreach ($err as $message) {
+            echo "<div class=\"alert danger\">".$message."</div>";
+        }
+    }
+}
+?>
 <!-- Add Cards -->
-<form class="view_form" action="#" method="post">
-    <input placeholder="عنوان تسک" type="text" />
-    <input placeholder="توضیحات" type="text" />
-    <input placeholder="تاریخ ثبت" type="text" />
+<form class="view_form" action="./task_add.php" method="post">
+    <input name="title" placeholder="عنوان تسک" type="text" />
+    <input name="content" placeholder="توضیحات" type="text" />
+    <input name="created_at" placeholder="تاریخ ثبت" type="text" />
     <input type="radio" name="status" id="done" value="done"> <label for="done">انجام شده</label>
     <input type="radio" name="status" id="not_done" value="not_done"> <label for="not_done">انجام نشده</label>
     <button type="submit" name="add_task">افزودن تسک</button>
